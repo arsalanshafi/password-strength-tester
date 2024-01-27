0b929d1c41f13bacdfa60a1faf1619f32593ad2c
abuArsalan <arsalanshafi911@gmail.com>
2024-01-27 15:46:13 +0530

    commit

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-//something
+const input = document.querySelector("#pwd");
+const messages = Array.from(document.querySelectorAll("li"));
+
+input.addEventListener("input",() => {
+    let pwd = input.value;
+    if(pwd.length < 8) messages[0].classList.add("show");
+    else messages[0].classList.remove("show");
+
+    let up = new RegExp("[A-Z]");
+
+    if(up.test(pwd)) messages[1].classList.remove("show");
+    else messages[1].classList.add("show");
+
+    let n = new RegExp("[0-9]")
+    if(n.test(pwd)) messages[2].classList.remove("show");
+    else messages[2].classList.add("show");
+
+    let sp = new RegExp("[$&+,:;=?@#|'<>.^*()%!-]");
+    if(sp.test(pwd)) messages[3].classList.remove("show");
+    else messages[3].classList.add("show");
+
+    
+})
